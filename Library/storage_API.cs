22cd20ab62ccd2207d34449ccaf3de26bbd527a9
vanhien2002 <tranvanhien19092002@gmail.com>
2023-09-23 14:58:20 +0700

    hoan thanh uc khach hang

---
+++ Library/storage_API.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public static class storage_API
+    {
+        public class API_KhachHang
+        {
+            private string _getAllKH = @"https://localhost:44327/api/khach-hang/get-all";
+            private string _getKHBySDT = @"https://localhost:44327/api/khach-hang/get-by-sdt/";
+            private string _deleteKhachHang = @"https://localhost:44327/api/khach-hang/remove-by-ma-khach-hang";
+            private string _insertKhachHang = "https://localhost:44327/api/khach-hang/add-item";
+            private string _updateKhachHang = @"https://localhost:44327/api/khach-hang/update";
+            public string getAllKH
+            {
+                get { return _getAllKH; }
+                set { }
+            }
+            public string getKHBySDT
+            {
+                get { return _getKHBySDT; }
+                set { }
+            }
+            public string deleteKhachHang
+            {
+                get { return _deleteKhachHang; }
+                set { }
+            }
+            public string insertKhachHang
+            {
+                get { return _insertKhachHang; }
+                set { }
+            }
+            public string updateKhachHang
+            {
+                get { return _updateKhachHang; }
+                set { }
+            }
+            
+        }
+    }
+}
---
+++ WinformQuanLyKho/GUI/FrmKhachHang/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WinformQuanLyKho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uC_KhachHang1 = new FrmUCLibrary.UC_KhachHang();
+            this.SuspendLayout();
+            // 
+            // uC_KhachHang1
+            // 
+            this.uC_KhachHang1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.uC_KhachHang1.Location = new System.Drawing.Point(12, 12);
+            this.uC_KhachHang1.Name = "uC_KhachHang1";
+            this.uC_KhachHang1.Size = new System.Drawing.Size(1036, 552);
+            this.uC_KhachHang1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 579);
+            this.Controls.Add(this.uC_KhachHang1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FrmUCLibrary.UC_KhachHang uC_KhachHang1;
+    }
+}
+
